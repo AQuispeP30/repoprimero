cf55e332d4c26c51b5c27142a34a7f93f0682e85
AlexQuispePillaca <alexqp1996@gmail.com>
2025-06-14 17:35:02 -0500

    agragar futter

---
+++ src/app/components/futter/futter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-futter',
+  imports: [],
+  templateUrl: './futter.component.html',
+  styleUrl: './futter.component.css'
+})
+export class FutterComponent {
+
+}
